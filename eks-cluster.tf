aa80434f51fde542cacb633810b6d6b4703b1c89
EC2 Default User <ec2-user@ip-172-31-45-186.ap-south-1.compute.internal>
2021-07-28 10:56:20 +0000

    Terraform with EKS

---
+++ eks-cluster.tf
@@ -0,0 +1,49 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-state-mindway-cluster"
+	key    = "states/terraform"
+    region = "eu-west-2"
+  }
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.20"
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    Environment = "dev"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-dev-1"
+      instance_type                 = "m5.4xlarge"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "master-group-1"
+      instance_type                 = "t3.large"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
